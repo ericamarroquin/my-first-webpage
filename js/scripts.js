43e488a76c61bae52622065d9e0f5defe230c506
EricaMarroquin <ericamarroquin03@gmail.com>
2021-08-10 14:45:22 -0700

    add functionality to dark/light button
    Co-authored-by: Scott Hutley <scotthutley1@comcast.net>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-  $("button#darkLight").click(function() {
+  $("button#lightButton").click(function() {
     $("body").removeClass();
     $("body").addClass("light-background");
+    $("button#lightButton").hide();
+    $("button#darkButton").show();
   });
-    
+  $("button#darkButton").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+    $("button#darkButton").hide();
+    $("button#lightButton").show();
+  });
 });
 
